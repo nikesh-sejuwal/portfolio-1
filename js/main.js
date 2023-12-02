7a6f9b6aa577367bd72b8b5c7a1b938e0ad9f976
nikesh-sejuwal <125644709+nikesh-sejuwal@users.noreply.github.com>
2023-12-02 19:06:43 +0545

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// Sticky menu background
+window.addEventListener("scroll", function () {
+   if (window.scrollY > 150) {
+      document.querySelector("#navbar").style.opacity = 0.7;
+   } else {
+      document.querySelector("#navbar").style.opacity = 1;
+   }
+});
+
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function (event) {
+   if (this.hash !== "") {
+      event.preventDefault();
+
+      const hash = this.hash;
+
+      $("html, body").animate(
+         {
+            scrollTop: $(hash).offset().top - 100,
+         },
+         800
+      );
+   }
+});
+
+const menu_btn = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector(".mobile-nav");
+// const mobile_men = document.querySelector(".mobile-na");
+menu_btn.addEventListener("click", function () {
+   menu_btn.classList.toggle("is-active");
+   mobile_menu.classList.toggle("is-active");
+   //  mobile_men.classList.toggle("is-active");
+});
