6b1d62f0ed3b7a6494ea28b41cd836b75d308e5f
nikesh-sejuwal <125644709+nikesh-sejuwal@users.noreply.github.com>
2023-12-03 08:30:11 +0545

    ADDED: auto hiding navbar

--- js/main.js
+++ js/main.js
@@ -26,6 +26,12 @@ $("#navbar a, .btn").on("click", function (event) {
 const menu_btn = document.querySelector(".hamburger");
 const mobile_menu = document.querySelector(".mobile-nav");
 // const mobile_men = document.querySelector(".mobile-na");
+mobile_menu.addEventListener("click", function () {
+   menu_btn.classList.toggle("is-active");
+   mobile_menu.classList.toggle("is-active");
+   //  mobile_men.classList.toggle("is-active");
+});
+
 menu_btn.addEventListener("click", function () {
    menu_btn.classList.toggle("is-active");
    mobile_menu.classList.toggle("is-active");
